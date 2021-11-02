bfb846ca08ae430d05a0e522f5d79ced16e4f114
lucasSCsantos <luc.cristovam10@gmail.com>
2021-11-02 18:15:38 -0300

    install and applicate cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const ToDos = require('../controllers/ToDos');
 
 const app = express();
